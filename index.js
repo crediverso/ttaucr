b1210a76f71ee0fd7606d0853db20f6ba741892c
Adrian Legaspi <alegaspi@artedigital-mx.com>
2021-10-26 16:29:28 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = class TTAUCR {
+    constructor() {
+        this.init = true;
+    }
+
+    get classInitiated() {
+        return this.init;
+    }
+
+    returnTrue() {
+        return true;
+    }
+} 
